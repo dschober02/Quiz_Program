9814a474739b92bda3f4a74947aecab8a27f3202
ducky1129 <125902700+ducky1129@users.noreply.github.com>
2024-09-27 10:21:27 -0500

    Initial commit

---
+++ Quiz Program/script.js
@@ -0,0 +1,103 @@
+const questions = [
+    {
+        question: 'What is the best sport?',
+        answers: [
+            { text: 'Soccer', correct: false },
+            { text: 'Basketball', correct: false },
+            { text: 'Baseball', correct: false },
+            { text: 'Volleyball', correct: true },
+        ]
+    },
+    {
+        question: 'Who is the president?',
+        answers: [
+            { text: 'Boe Jiden', correct: false },
+            { text: 'Donald Trump', correct: false },
+            { text: 'Futher Mocker', correct: false },
+            { text: 'Joe Biden', correct: true },
+        ]
+    },
+
+];
+
+const questionElement = document.getElementById('question');
+const answerButtons = document.getElementById('answer-buttons');
+const nextButton = document.getElementById('nextButton');
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer)
+    });
+}
+function resetState(){
+    nextButton.style.display = "none";
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedButton = e.target;
+    const isCorrect = selectedButton.dataset.correct === "true";
+    if(isCorrect){
+        selectedButton.classList.add("correct");
+        score++;
+    }else{
+        selectedButton.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+nextButton.addEventListener("click", ()=> {
+    if ( currentQuestionIndex < questions.length ) {
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+});
+
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else {
+        showScore();
+    }
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play again";
+    nextButton.style.display = "block";
+}
+
+startQuiz();
